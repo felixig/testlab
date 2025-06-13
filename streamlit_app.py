474f9187ff5b38dd17283dd2f7bab14469e298bf
felixig <fiv@gmx.at>
2025-06-13 20:59:40 +0200

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,18 +6,19 @@ users_df = pd.read_csv("users.csv")
 
 st.title("Login")
 
-username = st.text_input("Usuario")
-password = st.text_input("Contraseña", type="password")
+username = st.text_input("User")
+password = st.text_input("Password", type="password")
 
-if st.button("Entrar"):
+if st.button("Login"):
     user = users_df[(users_df["username"] == username) & (users_df["password"] == password)]
     if not user.empty:
-        st.success("Login correcto")
+        st.success("Right credentials")
         filename = user.iloc[0]["file"]
         with open(filename, "r") as f:
             content = f.read()
-        st.text_area("Archivo asignado", content, height=200)
+        st.text_area("Assigned file", content, height=200)
         # Para descarga:
-        st.download_button("Descargar archivo", data=content, file_name=filename)
+        st.download_button("Download file", data=content, file_name=filename)
     else:
-        st.error("Usuario o contraseña incorrectos")
+        st.error("Wrong user or password!")
+
